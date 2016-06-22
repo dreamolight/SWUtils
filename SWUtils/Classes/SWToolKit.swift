a77f715cfe7ad0c63aa3a46505151d59d93fa185
Stan Wu <stan.wu@me.com>
2016-06-22 23:56:34 +0800

    remove class

--- SWUtils/Classes/SWToolKit.swift
+++ SWUtils/Classes/SWToolKit.swift
@@ -27,27 +27,3 @@ public func sw_dispatch_on_background_thread(block: dispatch_block_t){
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),block)
     }
 }
-
-class SWUtils{
-    class func RunOnMainThread(block: dispatch_block_t){
-        if NSThread.isMainThread(){
-            block()
-        }else{
-            dispatch_sync(dispatch_get_main_queue(), block)
-        }
-    }
-    
-    class func RunOnBackgroundThread(block: dispatch_block_t){
-        if !NSThread.isMainThread(){
-            block()
-        }else{
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),block);
-        }
-    }
-    
-//    class func is_jailbroken() -> Bool{
-//        return UIApplication.sharedApplication().canOpenURL(NSURL(string: "cydia://package/com.example.package")!)
-//    }
-    
-    
-}
